9c30f86ceba85768ee76d57d74b60b1651b22c1c
mukhtar <mukhtarkabirmuhammad32@gmail.com>
2023-06-28 13:04:38 +0100

    Initialize files

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
